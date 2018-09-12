5b036a33ee5de4d68c845211b8b1fa5fdaa8628c
DevMMing <mming@stuy.edu>
2018-09-12 19:22:23 -0400

    added 02_randomizer

---
+++ 02_randomizer/hw02.py
@@ -0,0 +1,32 @@
+#BigBrainBoders -- Matthew Ming, Sajed Nahian
+#SoftDev1 pd6
+#K02 NO-body expects the Spanish Inquisition!
+#2018-9-11
+
+import random
+#allows us to use functions for randomization
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+
+}
+
+teams=['w','m','x']
+
+def randomStudent():
+    choice=input("Choose a team:w,m, or x");
+    #checks if a valid team has been chosen
+    if choice in teams:
+        students=KREWES[choice]
+        print (students[random.randint(0,len(students)-1)])
+    #function recurses till a proper input is made
+    else:
+        print ("An invalid team has been inputed. Please try again.")
+        randomStudent()
+        
+randomStudent()
