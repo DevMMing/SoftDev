73ca15ae98a85da13d5aa0a2f1d47a0f27ebb087
DevMMing <mming@stuy.edu>
2018-12-19 22:08:49 -0500

    minor changes

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -1,4 +1,4 @@
-// team unknown -- Britni Canale
+// team unknown -- Britni Canale, Matthew Ming
 // SoftDev pd6
 // K28 -- Sequential Progression II: Electric Boogaloo
 // 2018-12-19
@@ -51,17 +51,26 @@ var randomStudent=function(x){
 	var students=["Shelly","Nelson","Raymond","Yang","Julia","Garfield"];
 	return students[parseInt(Math.random()*students.length)];
 }
+var fibclick = function(){
 var fibbut = document.getElementById("fib");
 var fibber=Math.floor(Math.random()*35)
 var fibResult=fibonacci(fibber)
 fibbut.innerHTML="fib("+fibber.toString()+")="+fibResult.toString();
-fibbut.addEventListener("click",fibonacci);
+console.log(fibbut.innerHTML);
+}
+fibbut.addEventListener("click",fibclick);
+var gcdclick = function(){
 var gcdbut = document.getElementById("gcd");
 var num1=Math.floor(Math.random()*99);
 var num2=Math.floor(Math.random()*99);
 var gcdResult=gcd(num1,num2);
 gcdbut.innerHTML="gcd of "+num1.toString()+" and "+num2+" is " +gcdResult.toString();
-gcdbut.addEventListener("click",gcd);
+console.log(gcdbut.innerHTML);
+}
+gcdbut.addEventListener("click",gcdclick);
+var randclick = function(){
 var randbut=document.getElementById("rand");
 randbut.innerHTML=randomStudent();
-randbut.addEventListener("click",randomStudent);
+console.log(randbut.innerHTML);
+}
+randbut.addEventListener("click",randclick);
